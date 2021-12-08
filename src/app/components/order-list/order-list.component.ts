3a34ce0773aece071b333b177f6c0bab6c566047
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-12-08 22:32:03 +0700

    update Order list

--- src/app/components/order-list/order-list.component.ts
+++ src/app/components/order-list/order-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order.class';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
 })
 export class OrderListComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  public orders: Order[];
   ngOnInit(): void {
+    this.orders = JSON.parse(localStorage.getItem('orders')) || [];
   }
-
 }
