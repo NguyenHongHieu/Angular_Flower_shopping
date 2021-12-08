432d640e4f21732d84dc520c7c0eb1cfd176735f
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2021-12-08 21:03:57 +0700

    update product-detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Flower } from '../models/flower.class';
+import { CallToastService } from '../services/call-toast.service';
 import { FlowerService } from '../services/flower.service';
 
 @Component({
@@ -11,7 +12,10 @@ export class ProductDetailComponent implements OnInit {
   public flowers: Flower[] = [];
   public bags: number[] = [];
 
-  constructor(private _flowerService: FlowerService) {}
+  constructor(
+    private _flowerService: FlowerService,
+    private _toastService: CallToastService
+  ) {}
   @Output('idFlower')
   onHandleBags = new EventEmitter<number>();
 
@@ -21,6 +25,18 @@ export class ProductDetailComponent implements OnInit {
     console.log(this.flowers);
   }
   updateBag(id: number): void {
-    this.onHandleBags.emit(id);
+    // this.onHandleBags.emit(id);
+    //
+    var numberItem = this.bags.filter((x) => x == id).length;
+    var isOutOfStock = this._flowerService.IsOutOfStock(id, numberItem);
+
+    if (isOutOfStock) {
+      this._toastService.error('Exceeded', 2000);
+      return;
+    }
+
+    this.bags.push(id);
+    localStorage.setItem('bags', JSON.stringify(this.bags));
+    this._toastService.success('Added', 1000);
   }
 }
